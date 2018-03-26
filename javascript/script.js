3ed80f8d36f6bc0968fa9b86ac3399dd7de01d40
markjna <mentalsuicide@mail.ru>
2018-03-27 01:28:18 +0300

    My first exercise in js))))))))))))))))

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
-var userNAME = prompt("What is your name?");
-alert("Nice to meet you, " + userNAME);
-console.log("Also great to meet you, " + userNAME);
+var userFirstNAME = prompt("What is your First name?");
+var userLastNAME = prompt("What is your Last name?");
+var ageOld = prompt("How old are u?");
+var fullName = userFirstNAME + " " + userLastNAME;
+alert("Nice to meet you, " + fullName);
+console.log("Your full name is " + fullName);
+console.log("You are " + ageOld + " years old");
