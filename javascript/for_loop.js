47223eb68c7a994377951b0b1f081cc69e24e43e
markjna <mentalsuicide@mail.ru>
2018-03-31 19:09:41 +0300

    The same exercise that i have done yet but now made it with for loops

---
+++ javascript/for_loop.js
@@ -0,0 +1,16 @@
+for (var num = -10; num <= 19; num++){
+	console.log(num);
+}
+for (var num = 10; num <= 40; num+=2){
+	console.log(num);
+}
+for (var num = 300; num <= 333; num++){
+	if (num % 2 !== 0){
+		console.log(num);
+	}
+}
+for (var num = 5; num <= 50; num++){
+	if (num % 5 === 0 && num % 3 === 0){
+		console.log(num);
+	}
+}
