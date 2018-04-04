2cc5160f2d77e46f47aaa4878dc818fc2b4bddc3
markjna <mentalsuicide@mail.ru>
2018-04-04 21:00:06 +0300

    start making test

---
+++ javascript/test.js
@@ -0,0 +1,15 @@
+function printReverse() {
+	var num = [1,2,3,4];
+	num.reverse();
+	num.forEach(function(array){
+		console.log(array);
+	})
+}
+
+function newPrintReverse() {
+	var i = ["a", "b", "c"];
+	i.reverse();
+	i.forEach(function(array){
+		console.log(array);
+	})
+}
