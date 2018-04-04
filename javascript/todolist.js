5e212eacadfe8a270b0d446a9bb35abdb74bd27f
markjna <mentalsuicide@mail.ru>
2018-04-04 20:14:40 +0300

    add forEach ans splice

--- javascript/todolist.js
+++ javascript/todolist.js
@@ -1,12 +1,38 @@
 var input = prompt("What would u like to do?");
 var todos = [];
-while (input !== "quit") {
-	if (input === "list") {
-		console.log(todos);
-	}
-	else if (input === "new") {	
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+while (input !== "quit") 
+{
+	if (input === "list") 
+	{
+		listTodo();
+	} else if (input === "new") 
+	{	
+		allNewTodo();
+	} else if(input === "delite") 
+	{
+		allDeliteTodo();
 	}
 	input = prompt("What would u like to do?");
+}
+console.log("See u later!");
+function listTodo()
+{
+	console.log("******************");
+	todos.forEach(function(todo, index) 
+	{
+		console.log(index + ": " + todo);
+	});
+	console.log("******************");
+}
+function allNewTodo()
+{
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Added " + newTodo);
+}
+function allDeliteTodo()
+{
+	var deliteTodo = prompt("Type index of todo u wanna to delite");
+	todos.splice(deliteTodo, 1);
+	console.log("Delited index " + deliteTodo);
 }
