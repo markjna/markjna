c36bb1593b72be473ab665752f0ff21925fd8f4f
markjna <mentalsuicide@mail.ru>
2018-04-05 21:45:29 +0300

    movie databse

--- javascript/movie.js
+++ javascript/movie.js
@@ -1,25 +1,4 @@
-// var movie = 
-// {
-// 	cinema:
-// 	[
-// 		{name: "In Bruges"},
-// 		{stars: "5 stars"}
-// 	]
-// 	[
-// 		{name: "Frozen"},
-// 		{stars: "4,5 stars"}
-// 	]
-// 	[
-// 		{name: "Mad Max Fury Road"},
-// 		{stars: "5 stars"}
-// 	]
-// 	[
-// 		{name: "Les Miserables"},
-// 		{stars: "3,5 stars"}
-// 	]
-// }
-
-var movie = 
+var movies = 
 [
 	{
 	name: "In Bruges",
@@ -46,16 +25,42 @@ var movie =
 // console.log("You have watched " +  movie[i].name + " - " + movie[i].stars + " stars");
 // console.log("You have not watched " +  movie[i].name + " - " + movie[i].stars + " stars");
 
-for (var i = 0; i < movie.length; i++) 
-{	
+// function movieDB(movie)
+// {
+// 	for (var i = 0; i < movie.length; i++) 
+// 	{	
+// 		var result = "You have ";
+// 		if (movie[i].watched)
+// 		{
+// 			result += "watched ";
+// 		} else
+// 		{
+// 			result += "not seen ";
+// 		}
+// 		result += "\"" + movie[i].name + "\" - " + movie[i].stars + " stars";
+// 		console.log(result);
+// 	}
+// }
+
+
+// Another solution with function forEach()
+
+function movieDB(movie)
+{
 	var result = "You have ";
-	if (movie[i].watched)
+	if (movie.watched)
 	{
 		result += "watched ";
 	} else
 	{
 		result += "not seen ";
 	}
-	result += "\"" + movie[i].name + "\" - " + movie[i].stars + " stars";
-	console.log(result);
+	result += "\"" + movie.name + "\" - ";
+	result += movie.stars + " stars";
+	return result;
 }
+
+movies.forEach(function(movie)
+{
+	console.log(movieDB(movie));
+});
